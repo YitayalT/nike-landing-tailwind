f96fe21b5b9646d65e57ea7ed19d2b2f5d2f9035
YitayalT <anex518@gmail.com>
2023-08-18 17:00:35 -0330

    Adding hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,23 @@
-
+import Button from "../components/Button";
+import {arrowRight} from '../assets/icons'
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  border-2 border-red-500 p-2"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:padding-x pt-28">
+        <p>Our summer collection</p>
+        <h1>
+          <span>The New Arrival </span>
+          <br />
+          <span>Nike Shoes</span>
+        </h1>
+        <p>Discover stylish nike arrivals, quality comfort and innovation for your active life.</p>
+       <Button  label = 'Shop Now' iconURL = {arrowRight}/>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
